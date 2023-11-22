05716cd14a6fff602cc31e5bd5be52a91135e3cf
임수영 <99115114+ImSwim@users.noreply.github.com>
2023-11-22 12:30:15 +0900

    Add files via upload
    1122 12:30

---
+++ model.py
@@ -0,0 +1,51 @@
+from sqlalchemy import Column, TEXT, INT, BIGINT, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Booth(Base):
+    __tablename__ = "booth"
+
+    boothid = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
+    name = Column(TEXT, nullable=False)
+    boothcode = Column(INT, nullable=False)
+
+class Menu(Base):
+    __tablename__ = "menu"
+
+    menuid = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
+    boothid = Column(BIGINT, ForeignKey("booth.boothid"), nullable=False)
+    name = Column(TEXT, nullable=False)
+    price = Column(INT, nullable=False)
+
+class Order_Menu(Base):
+    __tablename__ = "order_menu"
+
+    order_menuid = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
+    orderid = Column(BIGINT, ForeignKey("order.orderid"), nullable=False)
+    menuid = Column(BIGINT, ForeignKey("menu.menuid"), nullable=False)
+    boothid = Column(BIGINT,  ForeignKey("booth.boothid"), nullable=False)
+
+###########
+
+class User(Base):
+    __tablename__ = "user"
+
+    userid = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
+    email = Column(TEXT, nullable=False)
+    password = Column(INT, nullable=False)
+    bank = Column(TEXT, nullable=True)
+    bankaccount = Column(TEXT, nullable=True)
+    bankbalance = Column(INT, nullable=True)
+
+class Staff(Base):
+    __tablename__ = "staff"
+
+    staffid = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
+    boothid = Column(BIGINT, nullable=False)
+    email = Column(TEXT, nullable=False)
+    password = Column(INT, nullable=False)
+    bank = Column(TEXT, nullable=True)
+    bankaccount = Column(TEXT, nullable=True)
+    bankbalance = Column(INT, nullable=True)
