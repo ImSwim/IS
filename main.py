26da6463f002deaf3abaf8ca70dd04dc6a283c7c
ImSwim <icanswim@khu.ac.kr>
2023-11-26 23:57:06 +0900

    장혜린 파트

--- main.py
+++ main.py
@@ -1,38 +1,94 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from dbconnection import engineconn
-from model import Booth, Menu, Order_Menu
+from pydantic import BaseModel
+from sqlalchemy import update
+from model import Booth, Menu, Order_Menu, User, Staff
 
 app: FastAPI = FastAPI() # FastAPI 모듈
 
 engine = engineconn()
 session = engine.sessionmaker()
 
-@app.api_route('/', methods=['GET', 'DELETE'])
+@app.get('/')
 async def main():
-    return {'success'}
+    return {'results': 'success'}
 
 '''GET'''
 
-@app.get("/booth")
+# 활성화된 부스 목록 확인하기
+@app.get("/get/booth")
 async def getBooth():
-    booth = session.query(Booth).all()
+    booth = session.query(Booth).filter(Booth.state == 1).all()
     return booth
 
-@app.get("/menu/{boothId}")
+# 특정 부스의 메뉴 목록 확인하기
+@app.get("/menu/get/{boothId}")
 async def getMenu(boothId: int):
     menu = session.query(Menu).filter(Menu.boothid == boothId).all()
     return menu
 
-@app.get("/order_menu/{boothId}")
+#주문 불이행 목록 확인하기
+@app.get("/order_menu/get/{boothId}")
 async def getOrder_Menu(boothId: int):
-    order_menu = session.query(Order_Menu).filter(Order_Menu.boothid == boothId).all()
+    order_menu = session.query(Order_Menu).filter((Order_Menu.boothid == boothId) & (Order_Menu.state == 0)).all()
     return order_menu
 
 
-'''DELETE'''
+'''INSERT'''
 
-@app.delete("/delete/{menuId}")
+# 주문 내역 DB에 넣기
+class Order_Menu(BaseModel):
+    userid : int
+    boothid : int
+    orderid : int
+    menuid : int
+    amount : int
+    state : int
+
+@app.post("/insert/order_menu")
+async def insertOrder_Menu(order_menu: Order_Menu):
+    try:
+        session.add(order_menu)
+        session.commit()
+    except Exception as e:
+        return {"message": f"Error inserting order menu: {str(e)}"}
+
+    return {"message": "success"}
+
+
+'''delete'''
+
+# 메뉴 삭제하기
+@app.delete("/delete/menu/{menuId}")
 async def deleteMenu(menuId : int):
-    session.query(Menu).filter(Menu.menuid == menuId).delete()
+    menu = session.query(Menu).filter(Menu.menuid == menuId).first()
+    if not menu:
+        return {"message": f"there is no menu id : {menuId}"}
+    session.delete(menu)
     session.commit()
-    return {"message": "Menu deleted successfully"}
+
+    return {"message": 'success'}
+
+'''Update'''
+
+# 주문 이행 상태 업데이트
+@app.get("/update/order_menu/{order_menuId}")
+async def updateordermenu(order_menuId : int) :
+    upordermenu = session.query(Order_Menu).filter(Order_Menu.order_menuid == order_menuId).first()
+    if not upordermenu :
+        return {"message": f"there is no order id: {order_menuId}"}
+    session.execute(update(Order_Menu).where(Order_Menu.order_menuid == order_menuId).values(state=1))
+    session.commit()
+
+    return {"message": 'success'}
+
+# 부스 상태 활성화 및 부스 아이디 반환
+@app.get("/boothstate/update/{boothCode}")
+async def updateordermenu(boothCode : int) :
+    booth = session.query(Booth).filter(Booth.boothcode == boothCode).first()
+    if not booth:
+        return {"message": f"there is no booth for {boothCode}"}
+    session.execute(update(Booth).where(Booth.boothid == boothCode).values(state=1))
+    session.commit()
+
+    return booth.boothid
--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, INT, BIGINT, ForeignKey
+from sqlalchemy import Column, String, Integer, BigInteger, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -7,45 +7,84 @@ Base = declarative_base()
 class Booth(Base):
     __tablename__ = "booth"
 
-    boothid = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
-    name = Column(String(collation="utf8mb4_unicode_ci"), nullable=False)
-    boothcode = Column(INT, nullable=False)
+    boothid = Column(BigInteger, nullable=False, autoincrement=True, primary_key=True)
+    name = Column(String, nullable=False)
+    boothcode = Column(Integer, nullable=False)
+    state = Column(Integer, nullable=False)
+
+    # 인용당함
+    staffs = relationship('Staff', back_populates='booths')
+    orders = relationship('Order', back_populates='booths')
+    menus = relationship('Menu', back_populates='booths')
+    order_menus = relationship('Order_Menu', back_populates='booths')
 
 class Menu(Base):
     __tablename__ = "menu"
 
-    menuid = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
-    boothid = Column(BIGINT, ForeignKey("booth.boothid"), nullable=False)
-    name = Column(String(collation="utf8mb4_unicode_ci"), nullable=False)
-    price = Column(INT, nullable=False)
+    menuid = Column(BigInteger, nullable=False, autoincrement=True, primary_key=True)
+    boothid = Column(BigInteger, ForeignKey("booth.boothid"), nullable=False)
+    name = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+    # 인용함
+    booths = relationship('Booth', back_populates='menus')
+    # 인용 당함
+    order_menus = relationship('Order_Menu', back_populates='menus')
+
+class Order(Base):
+    __tablename__ = "order"
+
+    orderid = Column(BigInteger, nullable=False, autoincrement=True, primary_key=True)
+    boothid = Column(BigInteger,  ForeignKey("booth.boothid"), nullable=False)
+    userid = Column(BigInteger,  ForeignKey("user.userid"), nullable=False)
+    tablenumber = Column(Integer, nullable=False)
+    totalprice = Column(Integer, nullable=False)
+
+    # 인용함
+    booths = relationship('Booth', back_populates='orders')
+    users = relationship('User', back_populates='orders')
+
+    #인용 당함
+    order_menus = relationship('Order_Menu', back_populates='orders')
 
 class Order_Menu(Base):
     __tablename__ = "order_menu"
 
-    order_menuid = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
-    orderid = Column(BIGINT, ForeignKey("order.orderid"), nullable=False)
-    menuid = Column(BIGINT, ForeignKey("menu.menuid"), nullable=False)
-    boothid = Column(BIGINT,  ForeignKey("booth.boothid"), nullable=False)
+    order_menuid = Column(BigInteger, nullable=False, autoincrement=True, primary_key=True)
+    orderid = Column(BigInteger, ForeignKey("order.orderid"), nullable=False)
+    menuid = Column(BigInteger, ForeignKey("menu.menuid"), nullable=False)
+    boothid = Column(BigInteger,  ForeignKey("booth.boothid"), nullable=False)
+    amount = Column(Integer, nullable=False)
+    state = Column(Integer, nullable=False)
 
+    # 인용함
+    booths = relationship('Booth', back_populates='order_menus')
+    menus = relationship('Menu', back_populates='order_menus')
+    orders = relationship('Order', back_populates='order_menus')
 ###########
 
 class User(Base):
     __tablename__ = "user"
 
-    userid = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
-    email = Column(String(collation="utf8mb4_unicode_ci"), nullable=False)
-    password = Column(INT, nullable=False)
-    bank = Column(String(collation="utf8mb4_unicode_ci"), nullable=True)
-    bankaccount = Column(String(collation="utf8mb4_unicode_ci"), nullable=True)
-    bankbalance = Column(INT, nullable=True)
+    userid = Column(BigInteger, nullable=False, autoincrement=True, primary_key=True)
+    email = Column(String, nullable=False)
+    password = Column(Integer, nullable=False)
+    bank = Column(String, nullable=True)
+    bankaccount = Column(String, nullable=True)
+    bankbalance = Column(Integer, nullable=True)
+
+    # 인용 당함
+    orders = relationship('Order', back_populates='users')
 
 class Staff(Base):
     __tablename__ = "staff"
 
-    staffid = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
-    boothid = Column(BIGINT, nullable=False)
-    email = Column(String(collation="utf8mb4_unicode_ci"), nullable=False)
-    password = Column(String(collation="utf8mb4_unicode_ci"), nullable=False)
-    bank = Column(String(collation="utf8mb4_unicode_ci"), nullable=True)
-    bankaccount = Column(String(collation="utf8mb4_unicode_ci"), nullable=True)
-    bankbalance = Column(INT, nullable=True)
+    staffid = Column(BigInteger, nullable=False, autoincrement=True, primary_key=True)
+    boothid = Column(BigInteger, ForeignKey("booth.boothid"), nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    bank = Column(String, nullable=True)
+    bankaccount = Column(String, nullable=True)
+    bankbalance = Column(Integer, nullable=True)
+    priority = Column(Integer, nullable=False)
+
+    booths = relationship('Booth', back_populates='staffs')
